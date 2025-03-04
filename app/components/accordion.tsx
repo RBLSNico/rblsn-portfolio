6de9ecd84278070e22d6292f8449ceafd16cf1ec
Nico <nicorobles122882@gmail.com>
2025-03-04 17:23:46 +0800

    added section

---
+++ app/components/accordion.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel";
+import { IoMdClose } from "react-icons/io";
+
+
+const AccordionWithCarousel = () => {
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+    // Function to close modal
+    const closeModal = () => setSelectedImage(null);
+
+    return (
+        <>
+            {/* Accordion */}
+            <Accordion type="single" collapsible className="text-white px-5">
+                <AccordionItem value="item-1">
+                    <AccordionTrigger className="cursor-pointer">PageSpeed Optimization</AccordionTrigger>
+                    <AccordionContent>
+                        <Carousel>
+                            <CarouselContent className="flex-col md:flex-row">
+                                <CarouselItem className="basis-1/2">
+                                    <h2 className="text-center py-2">Before</h2>
+                                    <img
+                                        src="/image/faha_metrix_before.png"
+                                        alt="sample works"
+                                        className="rounded-xl w-full h-auto cursor-pointer"
+                                        onClick={() => setSelectedImage("/image/faha_metrix_before.png")} // Open modal on click
+                                    />
+                                </CarouselItem>
+                                <CarouselItem className="basis-1/2">
+                                    <h2 className="text-center py-2">After</h2>
+                                    <img
+                                        src="/image/faha_metrix_after.png"
+                                        alt="sample works"
+                                        className="rounded-xl w-full h-auto cursor-pointer"
+                                        onClick={() => setSelectedImage("/image/faha_metrix_after.png")} // Open modal on click
+                                    />
+                                </CarouselItem>
+                            </CarouselContent>
+                            <CarouselPrevious />
+                            <CarouselNext />
+                        </Carousel>
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+            {/* Full-Screen Modal */}
+            {selectedImage && (
+                <div
+                    className="fixed inset-0 bg-gray/20 backdrop-blur-sm bg-opacity-50 flex justify-center items-center z-50"
+                    onClick={closeModal} // Close when clicking outside
+                >
+                    <div>
+                        <IoMdClose className="float-end cursor-pointer w-10" onClick={closeModal} />
+                        <img
+                            src={selectedImage}
+                            alt="Full Screen"
+                            className="max-w-full max-h-full rounded-xl shadow-lg"
+                        />
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default AccordionWithCarousel;
--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useRef, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import { FaGithub, FaJava, FaAndroid, FaHtml5, FaAngular, FaDatabase, FaWordpress, FaReact, FaBootstrap, FaElementor, FaCloudflare, FaCcStripe, FaFigma, FaBriefcase, FaLaptopCode } from "react-icons/fa";
 import { SiKotlin, SiTypescript, SiTailwindcss, SiOpencv, SiSwagger, SiPhp, SiPlesk, SiChartdotjs } from "react-icons/si";
 import { PiFileCSharp } from "react-icons/pi";
 import { RiNextjsFill } from "react-icons/ri";
 import { IoLogoJavascript } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 import { DiMsqlServer, DiMysql } from "react-icons/di";
 import { TbApi } from "react-icons/tb";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import AccordionItem from "./accordion";
 
 import {
     Carousel,
@@ -34,6 +36,7 @@ interface ProjectItem {
     id?: string;
     github?: string;
     link?: string;
+    accordion?: ReactNode;
 }
 
 interface ProjectCardProps {
@@ -47,6 +50,8 @@ gsap.registerPlugin(ScrollTrigger);
 const ProjectsSection = () => {
     const projectsRef = useRef<HTMLDivElement[]>([]);
     const sectionsRef = useRef<HTMLDivElement[]>([]);
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+    const closeModal = () => setSelectedImage(null);
 
     useEffect(() => {
         projectsRef.current.forEach((project, index) => {
@@ -95,7 +100,7 @@ const ProjectsSection = () => {
             description:
                 "As the lead WordPress Developer for FindaHost, I was tasked to build, optimize, and maintain the whole website, ensuring seamless functionality, Search Engine Optimization (SEO), and enhanced user experience.",
             position: "Wordress Developer",
-            images: ["/image/findahost1.png", "/image/findahost2.png", "/image/findahost3.png"],
+            images: ["/image/findahost1.png", "/image/findahost2.png", "/image/findahost3.png", "/image/fahalp1.png", "/image/fahalp2.png", "/image/fahalp3.png", "/image/fahalp4.png"],
             date: "June 2024 - Present",
             link: "https://findahost.io/",
             gradient: "bg-gradient-to-r from-purple-700 to-fuchsia-950",
@@ -109,7 +114,8 @@ const ProjectsSection = () => {
                 { icon: FaCcStripe, name: "Stripe" },
                 { icon: SiPlesk, name: "Plesk Obsidian" },
                 { icon: FaFigma, name: "Figma" },
-            ]
+            ],
+            accordion: <AccordionItem />
         },
         {
             title: "Kunda House",
@@ -231,131 +237,154 @@ const ProjectsSection = () => {
     // Project card component to avoid repetition
     const ProjectCard: React.FC<ProjectCardProps> = ({ item, index, isWorkExperience = false }) => {
         return (
-            <div
-                key={index}
-                ref={(el) => {
-                    if (el) projectsRef.current.push(el);
-                }}
-                className={`flex flex-col items-center justify-center gap-5 p-10 rounded-xl ${item.gradient} w-full ${isWorkExperience ? 'md:flex-row' : 'md:flex-row-reverse'}`}
-                id={item.id}
-            >
-                {/* Carousel Section */}
-                <div className="flex items-center justify-center md:w-1/2">
-                    <Carousel className="max-w-[85%]">
-                        <CarouselContent>
-                            {item.images.map((image: string, i: number) => (
-                                <CarouselItem key={i} className="flex justify-center">
-                                    <img src={image} alt={`${item.title} - ${i + 1}`} className="rounded-xl w-full h-auto" />
-                                </CarouselItem>
-                            ))}
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel>
-                </div>
-
-                {/* Project Info Section */}
-                <div className="w-full md:w-1/2 flex flex-col gap-3 items-center md:items-start">
-                    <h1 className="text-white text-2xl font-semibold text-center md:text-left">{item.title}</h1>
-                    <h2 className="text-white text-md font-normal text-center md:text-left">{item.position}</h2>
-                    <h2 className="text-white text-sm font-extralight text-center md:text-left">{item.date}</h2>
-                    <p className="text-white text-center md:text-left">{item.description}</p>
-
-                    {/* Technology Icons */}
-                    <div className="flex flex-wrap gap-4 justify-center md:justify-start mt-2">
-                        {item.icons.map((iconItem, i) => {
-                            const [showName, setShowName] = useState(false);
-
-                            return (
-                                <div
-                                    key={i}
-                                    className="flex flex-col items-center relative group cursor-pointer"
-                                    onClick={() => setShowName(!showName)}
-                                >
-                                    <iconItem.icon
-                                        size={24}
-                                        className="text-white hover:text-gray-200 transition-colors duration-300"
-                                    />
-                                    <span
-                                        className={`absolute bottom-[-20px] bg-gray-800 text-white text-xs px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 ${showName ? "opacity-100" : ""}`}
-                                    >
-                                        {iconItem.name}
-                                    </span>
-                                </div>
-                            );
-                        })}
+            <div className={`flex flex-col rounded-xl ${item.gradient}`}>
+                <div
+                    key={index}
+                    ref={(el) => {
+                        if (el) projectsRef.current.push(el);
+                    }}
+                    className={`flex flex-col items-center justify-center gap-5 p-10 w-full ${isWorkExperience ? 'md:flex-row' : 'md:flex-row-reverse'}`}
+                    id={item.id}
+                >
+                    {/* Carousel Section */}
+                    <div className="flex items-center justify-center md:w-1/2">
+                        <Carousel className="max-w-[85%]">
+                            <CarouselContent>
+                                {item.images.map((image: string, i: number) => (
+                                    <CarouselItem key={i} className="flex justify-center">
+                                        <img src={image} alt={`${item.title} - ${i + 1}`} className="rounded-xl w-full h-auto cursor-pointer" onClick={() => setSelectedImage(image)} />
+                                    </CarouselItem>
+                                ))}
+                            </CarouselContent>
+                            <CarouselPrevious />
+                            <CarouselNext />
+                        </Carousel>
                     </div>
 
-                    {isWorkExperience ? (
-                        <a
-                            href={item.link}
-                            className="flex items-center mt-2 w-fit bg-white text-black rounded-xl px-5 py-3 hover:bg-opacity-80 transition-colors duration-300 ease-in-out"
-                        >
-                            Visit Website
-                        </a>
-                    ) : (
-                        <a
-                            href={item.github}
-                            className="flex items-center mt-2 w-fit bg-white text-black rounded-xl px-5 py-3 hover:bg-opacity-80 transition-colors duration-300 ease-in-out"
-                        >
-                            <FaGithub className="mr-2" />
-                            View on GitHub
-                        </a>
-                    )}
+                    {/* Project Info Section */}
+                    <div className="w-full md:w-1/2 flex flex-col gap-3 items-center md:items-start">
+                        <h1 className="text-white text-2xl font-semibold text-center md:text-left">{item.title}</h1>
+                        <h2 className="text-white text-md font-normal text-center md:text-left">{item.position}</h2>
+                        <h2 className="text-white text-sm font-extralight text-center md:text-left">{item.date}</h2>
+                        <p className="text-white text-center md:text-left">{item.description}</p>
+
+                        {/* Technology Icons */}
+                        <div className="flex flex-wrap gap-4 justify-center md:justify-start mt-2">
+                            {item.icons.map((iconItem, i) => {
+                                const [showName, setShowName] = useState(false);
+
+                                return (
+                                    <div
+                                        key={i}
+                                        className="flex flex-col items-center relative group cursor-pointer"
+                                        onClick={() => setShowName(!showName)}
+                                    >
+                                        <iconItem.icon
+                                            size={24}
+                                            className="text-white hover:text-gray-200 transition-colors duration-300"
+                                        />
+                                        <span
+                                            className={`absolute bottom-[-20px] bg-gray-800 text-white text-xs px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 ${showName ? "opacity-100" : ""}`}
+                                        >
+                                            {iconItem.name}
+                                        </span>
+                                    </div>
+                                );
+                            })}
+                        </div>
+
+                        {isWorkExperience ? (
+                            <a
+                                href={item.link}
+                                className="flex items-center mt-2 w-fit bg-white text-black rounded-xl px-5 py-3 hover:bg-opacity-80 transition-colors duration-300 ease-in-out"
+                            >
+                                Visit Website
+                            </a>
+                        ) : (
+                            <a
+                                href={item.github}
+                                className="flex items-center mt-2 w-fit bg-white text-black rounded-xl px-5 py-3 hover:bg-opacity-80 transition-colors duration-300 ease-in-out"
+                            >
+                                <FaGithub className="mr-2" />
+                                View on GitHub
+                            </a>
+                        )}
+                    </div>
                 </div>
+                <span>{item.accordion}</span>
             </div>
         );
     };
 
     return (
-        <section id="projects" className="rounded-xl flex flex-col items-center gap-10">
-            {/* Work Experience Section Header */}
-            <div
-                ref={(el) => { if (el) sectionsRef.current[0] = el; }}
-                className="w-full flex flex-col items-center gap-3 mt-5"
-            >
-                {/* <div className="flex items-center gap-2 bg-blue-900 text-white px-6 py-3 rounded-full">
+        <>
+            <section id="projects" className="rounded-xl flex flex-col items-center gap-10">
+                {/* Work Experience Section Header */}
+                <div
+                    ref={(el) => { if (el) sectionsRef.current[0] = el; }}
+                    className="w-full flex flex-col items-center gap-3 mt-5"
+                >
+                    {/* <div className="flex items-center gap-2 bg-blue-900 text-white px-6 py-3 rounded-full">
                     <FaBriefcase size={20} />
                     <h1 className="text-white text-2xl font-bold">Work Experience</h1>
                 </div>
                 <div className="h-1 w-32 bg-blue-600 rounded-full"></div> */}
-                <h1 className="text-(--primary-blue) text-3xl font-bold text-center mt-5">Work Experience</h1>
-            </div>
-
-            {/* Work Experience Cards */}
-            <div className="flex flex-col items-center gap-8 w-full">
-                {workExperience.map((item, index) => (
-                    <ProjectCard key={index} item={item} index={index} isWorkExperience={true} />
-                ))}
-            </div>
-
-            {/* Divider between sections */}
-            <div className="w-full flex justify-center my-8">
-                <div className="relative w-full max-w-4xl h-px bg-gray-300">
+                    <h1 className="text-(--primary-blue) text-3xl font-bold text-center mt-5">Work Experience</h1>
                 </div>
-            </div>
 
-            {/* Projects Section Header */}
-            <div
-                ref={(el) => { if (el) sectionsRef.current[1] = el; }}
-                className="w-full flex flex-col items-center gap-3 mt-5"
-            >
-                {/* <div className="flex items-center gap-2 bg-green-800 text-white px-6 py-3 rounded-full">
+                {/* Work Experience Cards */}
+                <div className="flex flex-col items-center gap-8 w-full">
+                    {workExperience.map((item, index) => (
+                        <ProjectCard key={index} item={item} index={index} isWorkExperience={true} />
+                    ))}
+                </div>
+
+                {/* Divider between sections */}
+                <div className="w-full flex justify-center my-8">
+                    <div className="relative w-full max-w-4xl h-px bg-gray-300">
+                    </div>
+                </div>
+
+                {/* Projects Section Header */}
+                <div
+                    ref={(el) => { if (el) sectionsRef.current[1] = el; }}
+                    className="w-full flex flex-col items-center gap-3 mt-5"
+                >
+                    {/* <div className="flex items-center gap-2 bg-green-800 text-white px-6 py-3 rounded-full">
                     <FaLaptopCode size={20} />
                     <h1 className="text-white text-2xl font-bold">Projects</h1>
                 </div>
                 <div className="h-1 w-32 bg-green-600 rounded-full"></div> */}
-                <h1 className="text-(--primary-blue) text-3xl font-bold text-center">Projects</h1>
-            </div>
+                    <h1 className="text-(--primary-blue) text-3xl font-bold text-center">Projects</h1>
+                </div>
 
 
-            {/* Project Cards */}
-            <div className="flex flex-col items-center gap-8 w-full">
-                {projects.map((item, index) => (
-                    <ProjectCard key={index} item={item} index={index} isWorkExperience={false} />
-                ))}
-            </div>
-        </section>
+                {/* Project Cards */}
+                <div className="flex flex-col items-center gap-8 w-full">
+                    {projects.map((item, index) => (
+                        <ProjectCard key={index} item={item} index={index} isWorkExperience={false} />
+                    ))}
+                </div>
+            </section>
+
+
+            {/* Full-Screen Modal */}
+            {selectedImage && (
+                <div
+                    className="fixed inset-0 bg-gray/20 backdrop-blur-sm bg-opacity-50 flex justify-center items-center z-50"
+                    onClick={closeModal} // Close when clicking outside
+                >
+                    <div>
+                        <IoMdClose className="float-end cursor-pointer w-10" onClick={closeModal} />
+                        <img
+                            src={selectedImage}
+                            alt="Full Screen"
+                            className="max-w-full max-h-full rounded-xl shadow-lg"
+                        />
+                    </div>
+                </div>
+            )}
+        </>
     );
 };
 
